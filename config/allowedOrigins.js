60bdfe029234218798074b9a9cabd74c925d83d4
William Maroon <william.maroon@gmail.com>
2023-06-26 15:42:02 -0400

    added "back" to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,9 @@
 const allowedOrigins = [
     'https://clearancequick.onrender.com',
+    'https://clearancequickback.onrender.com',
     'https://www.clearancequick.com',
+    'https://www.clearancequickback.com',
+    'https://clearancequickback.com',
     'https://clearancequick.com'
 ]
 
