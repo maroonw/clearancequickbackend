deacacec3a4ed2e4a8214c63909dd8d22c0e2718
William Maroon <william.maroon@gmail.com>
2023-06-26 14:29:15 -0400

    changed user.js

--- models/User.js
+++ models/User.js
@@ -9,14 +9,14 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    roles: [{
-        type: String,
-        default: "Employee"
-    }],
+    roles: {
+        type: [String],
+        default: ["Employee"]
+    },
     active: {
         type: Boolean,
         default: true
-    },
+    }
 })
 
 module.exports = mongoose.model('User', userSchema)
